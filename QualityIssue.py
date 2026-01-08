5780acf8c2afcb0c0dc3bc6bbebdf9785a494ef5
apexhuang-zizi <apexhuang76@gmail.com>
2026-01-09 01:47:38 +0700

    QualityIssue.py

--- QualityIssue.py
+++ QualityIssue.py
@@ -134,11 +134,11 @@ if st.session_state.records:
             # 4. 生成并提供下载
             # 修改为这个写法：
             pdf_output = pdf.output()  # fpdf2 默认直接输出字节数组(bytearray)
-                 st.download_button(
-            label="✅ 确认生成并下载 PDF",
-            data=bytes(pdf_output), # 将其转换为字节
-            file_name=f"{p_id}_Report.pdf",
-            mime="application/pdf"
+            st.download_button(
+                label="✅ 确认生成并下载 PDF",
+                data=bytes(pdf_output), # 将其转换为字节
+                file_name=f"{p_id}_Report.pdf",
+                mime="application/pdf"
             ) 
             st.download_button(
                 label="✅ 确认生成并下载 PDF",
